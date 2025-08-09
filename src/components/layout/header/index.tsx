9b8a7495992574788030aa84b479695e248f75ca
kaunda-a <67536043+kaunda-a@users.noreply.github.com>
2025-08-09 22:25:19 +0200

    motion

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import React from 'react';
 import { Separator } from '../../ui/separator';
 import { Breadcrumbs } from '../../breadcrumbs';
 import SearchInput from '../../search-input';
 import { ThemeSelector } from '../../theme-selector';
 import { ModeToggle } from '../ThemeToggle/theme-toggle';
-import { motion } from 'motion';
+import { motion } from 'motion/react';
 
 export function Header() {
   return (
