3bae85dc3b3321b8a8014d99f0d7fec3eefb0533
kaunda-a <67536043+kaunda-a@users.noreply.github.com>
2025-08-22 00:07:05 +0200

    Fix syntax error in global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -8,14 +8,12 @@ export default function GlobalError({
   error: Error & { digest?: string };
 }) {
   return (
-    <html>
-      <body>
-        {/* `NextError` is the default Next.js error page component. Its type
-        definition requires a `statusCode` prop. However, since the App Router
-        does not expose status codes for errors, we simply pass 0 to render a
-        generic error message. */}
-        <NextError statusCode={0} />
-      </body>
-    </html>
+    <>
+      {/* `NextError` is the default Next.js error page component. Its type
+          definition requires a `statusCode` prop. However, since the App Router
+          does not expose status codes for errors, we simply pass 0 to render a
+          generic error message. */}
+      <NextError statusCode={0} />
+    </>
   );
 }
