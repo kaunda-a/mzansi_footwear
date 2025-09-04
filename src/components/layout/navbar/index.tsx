0be5379bb659216c57fe5d2ae2c6ceb3d279d77d
kaunda-a <67536043+kaunda-a@users.noreply.github.com>
2025-09-04 16:27:21 +0200

    Restore cart functionality to navbar

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import {
   IconHome,
   IconCategory,
+  IconShoppingCart,
   IconUser,
 } from "@tabler/icons-react";
 import { useCartStore } from "@/lib/cart-store";
@@ -49,6 +50,14 @@ export function Navbar() {
       icon: IconCategory,
       href: "/products",
       color: "text-purple-500",
+    },
+    {
+      id: "cart",
+      label: "Cart",
+      icon: IconShoppingCart,
+      href: "/cart",
+      badge: cartItemCount,
+      color: "text-green-500",
     },
     {
       id: "account",
