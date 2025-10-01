852e76c1806a3aa648d21b31e63cd6f0c17ec5eb
kaunda-a <67536043+kaunda-a@users.noreply.github.com>
2025-10-02 00:14:04 +0200

    fix: update Google Analytics implementation using next/script

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -1,7 +1,27 @@
 'use client';
 
-import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 
 export default function GoogleAnalyticsComponent() {
-  return <GoogleAnalytics gaId="G-HC1GMR7WTG" />;
+  return (
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-HC1GMR7WTG`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HC1GMR7WTG');
+          `,
+        }}
+      />
+    </>
+  );
 }
